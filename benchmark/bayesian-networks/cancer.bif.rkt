281288b51ad6723f6eabde7a3619faf0f0cb66b1
Smaran Teja <smaranteja32@gmail.com>
2026-07-17 21:34:14 -0400

    wipe clean

--- benchmark/bayesian-networks/cancer.bif.rkt
+++
@@ -1,8 +0,0 @@
-#lang roulette/example/disrupt
-
-(require "private/bayes.rkt")
-
-
-
-(benchmark
-      (main "bayesian-networks/cancer.bif" 'Xray `(Pollution Smoker Cancer Dyspnoea Xray)))
--- benchmark/small/two-coins.rkt
+++
@@ -1,8 +0,0 @@
-#lang roulette/example/disrupt
-
-
-(define first-coin (flip 0.5))
-(define second-coin (flip 0.5))
-(define both-heads (&& first-coin second-coin))
-(observe! (! both-heads))
-(benchmark first-coin)
