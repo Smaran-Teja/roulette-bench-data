20d1f3cb3b65de6bc7983d3280a82a85eb3678d4
Smaran Teja <smaranteja32@gmail.com>
2026-07-18 19:49:29 -0400

    wipe clean

--- benchmark/bayesian-networks/cancer.bif.rkt
+++
@@ -1,9 +0,0 @@
-#lang roulette/example/disrupt
-
-(require "../private/bayes.rkt" "../benchmarking.rkt")
-(provide main)
-
-
-(define (main)
-  (benchmark
-    (main* "bayesian-networks/cancer.bif" 'Xray `(Pollution Smoker Cancer Dyspnoea Xray))))
